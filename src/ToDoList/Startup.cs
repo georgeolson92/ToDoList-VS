9bda6efa47c521b8889b0bde8eaaa24c425ab75f
George Olson <georgeolson92@gmail.com>
2016-09-07 09:15:18 -0700

    Initial commit

--- src/ToDoList/Startup.cs
+++ src/ToDoList/Startup.cs
@@ -6,26 +6,41 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using ToDoList.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList
 {
     public class Startup
     {
+        public IConfigurationRoot Configuration { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            //1
             services.AddMvc();
-        }
 
+            services.AddEntityFramework()
+                .AddDbContext<ToDoListContext>(options =>
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+        }
         public void Configure(IApplicationBuilder app)
         {
-            //2
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
